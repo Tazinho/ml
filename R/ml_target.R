2d52cfd4cbde1b43aa08de7e8acaacae1b44bbf3
Tazinho <malte.grosser@gmail.com>
2018-02-16 21:19:25 +0100

    added ml_target + test

---
+++ R/ml_target.R
@@ -0,0 +1,36 @@
+#' Specify a target variable for a machine learning model
+#'
+#' @param data_sets_list A list containing at least one data frame \code{.train}
+#' and possibly other data frames \code{.test1} and \code{.test2}, as well as 
+#' other dot prefixed elements specified earlier in the pipeline.
+#' 
+#' @param target A character vector or NSE containing the target name, which 
+#' must be a column name of training and test sets within \code{data_sets_list}. 
+#' Alternatively a formula can be supplied, the right hand side elements are
+#' automaitcally known as .target and a call to \code{ml_target} is not 
+#' necessary anymore. Typically only one target should be set, but it is possible
+#' to create many models at once.
+#' 
+#' @return A list containing the train and test sets as well as a .target
+#' element and other elements prefixed by a dot, if supplied earlier in the
+#' pipeline.
+#' 
+#' @author Malte Grosser, \email{malte.grosser@@gmail.com}
+#' @keywords utilities
+#'
+#' @examples
+#' ml_features(data_sets_list = list(.train = iris), target = c("Petal.Width"))
+#'
+#' @seealso \href{https://github.com/Tazinho/ml}{ml on github}.
+#'
+#' @export
+#'
+ml_target <- function(data_sets_list, target){
+  # checks
+  
+  # prepare output
+  data_sets_list[[".target"]] <- target
+  
+  # return
+  return(data_sets_list)
+}
---
+++ tests/testthat/test_ml_target.R
@@ -0,0 +1,38 @@
+context("ml_features")
+
+test_that("target as character", {
+  expect_equal(ml_target(list(.train = iris[1:3, ]), target = "Species"),
+               structure(
+                 list(
+                   .train = structure(
+                     list(
+                       Sepal.Length = c(5.1,
+                                        4.9, 4.7),
+                       Sepal.Width = c(3.5, 3, 3.2),
+                       Petal.Length = c(1.4,
+                                        1.4, 1.3),
+                       Petal.Width = c(0.2, 0.2, 0.2),
+                       Species = structure(
+                         c(1L,
+                           1L, 1L),
+                         .Label = c("setosa", "versicolor", "virginica"),
+                         class = "factor"
+                       )
+                     ),
+                     .Names = c(
+                       "Sepal.Length",
+                       "Sepal.Width",
+                       "Petal.Length",
+                       "Petal.Width",
+                       "Species"
+                     ),
+                     row.names = c(NA,
+                                   3L),
+                     class = "data.frame"
+                   ),
+                   .target = "Species"
+                 ),
+                 .Names = c(".train",
+                            ".target")
+               ))
+})
